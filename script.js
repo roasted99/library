25b90f77651517951d26a1fef461541e06149333
roasted99 <shwegyimontkyut@gmail.com>
2021-10-21 13:17:47 +0630

    Add the book to the library and delete button

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 let myLibrary = [
     {
-        number: 1,
         title: '1984',
         author: 'George Orwell',
         pages: 298,
         isbn: 9780451524935
     },
     {
-        number: 2,
         title: 'The Alchemist',
         author: 'Paul Coelho',
         pages: 208,
@@ -38,12 +36,56 @@ function addBookToLibrary(book) {
     <td>${book.author}</td>
     <td>${book.pages}</td>
     <td>${book.isbn}</td>
-    <td>Read</td>
+    <td><input type="button" class="book-status" value="Read"></input></td>
     <td><button class="delete">X</button></td>
     `;
 
     list.appendChild(row);
 }
 
+function deleteBook(el) {
+    if (el.classList.contains('delete')) {
+        el.parentElement.parentElement.remove();
+    }
+}
+
+function clearField() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#pages').value = '';
+    document.querySelector('#isbn').value = '';
+}
+
 //Event to display book
 document.addEventListener('DOMContentLoaded', displayBook());
+
+//Event to add book
+document.querySelector('#book-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    const book = new Book(title, author, pages, isbn);
+
+    addBookToLibrary(book);
+
+    clearField();
+})
+
+//delete book
+document.querySelector('#book-list').addEventListener('click', e => {
+    deleteBook(e.target)
+})
+
+//change status
+const changeStatus = document.querySelectorAll('.book-status');
+changeStatus.forEach(change => change.addEventListener('click', (e) => {
+    if (e.target.value === 'Read') {
+        e.target.value = 'Not Read';
+    } else {
+        e.target.value = 'Read';
+    }
+}));
